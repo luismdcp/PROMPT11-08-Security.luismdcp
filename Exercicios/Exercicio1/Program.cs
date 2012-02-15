c728ccf3fdfeadf98e0a9900002614e5c02fe64a
luismdcp <luis.mdc.pereira@hotmail.com>
2012-02-15 19:27:19 +0000

    first commit

---
+++ Exercicios/Exercicio1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"c:\1.png";
+            byte[] fileContentBytes = File.ReadAllBytes(filePath);
+
+            SHA512Managed hashFunction = new SHA512Managed();
+            var hashBytes = hashFunction.ComputeHash(fileContentBytes);
+            var encoding = new UTF8Encoding();
+            var stringResult = encoding.GetString(hashBytes);
+        }
+    }
+}
